2de27a97266df539772c1bff27e249f130b4991b
Daniel Rotaru <daniel_rotaru@hotmail.com>
2023-04-14 11:19:55 +0200

    update

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -43,88 +43,92 @@ const Login = () => {
         return <Navigate to="/" />
     }
     return (
-        <section className="h-100 gradient-form">
-  <div className="container py-5 h-100">
-    <div className="row d-flex justify-content-center align-items-center h-100">
-      <div className="col-xl-10">
-        <div className="card rounded-3 text-black">
-          <div className="row g-0">
-            <div className="col-lg-6">
-              <div className="card-body p-md-5 mx-md-4">
+    <section className="h-100 gradient-form">
+    <div className="container py-5 h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-xl-10">
+          <div className="card rounded-3 text-black">
+            <div className="row g-0">
+              <div className="col-lg-6">
+                <div className="card-body p-md-5 mx-md-4">
 
-                <div classNameName="text-center">
-                  <img className="logoImage" src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                     alt="logo"/>
-                  <h4 className="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
+                  <div className="text-center">
+                    <img className="logoImage" src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                      alt="logo"/>
+                    <h4 className="mt-1 mb-5 pb-1">Spartans Chat</h4>
+                  </div>
+
+                  <form onSubmit={submitHandler}>
+                    <p>Please login to your spartan account</p>
+
+                    <div className="form-outline mb-4">
+                      <input type="email" id="form2Example11" className="form-control"
+                        placeholder="Enter your spartan worrior!" value={email}
+                        onChange={e => setEmail(e.target.value)}/>
+                      <label className="form-label" for="form2Example11">Username</label>
+                      <div classNameName="email error red-text">{emailError}</div>
+                    </div>
+
+                    <div className="form-outline mb-4">
+                      <input type="password" id="form2Example22" className="form-control" placeholder="Enter spartan warcry!" value={password}
+                        onChange={e => setPassword(e.target.value)}/>
+                      <label className="form-label" for="form2Example22">Password</label>
+                      <div classNameName="password error red-text">{passwordError}</div>
+                    </div>
+
+                    <div className="text-center pt-1 mb-5 pb-1">
+                      <button className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3">Log in</button>
+                    </div>
+
+                    <div className="d-flex align-items-center justify-content-center pb-4">
+                      <p className="mb-0 me-2">Don't have an account?</p>
+                      <button className="btn btn-outline-danger">
+                      <a href="/signup">Signup</a>
+                      </button>
+                    </div>
+
+                  </form>
+
                 </div>
-
-                <form>
-                  <p>Please login to your account</p>
-
-                  <div className="form-outline mb-4">
-                    <input type="email" id="form2Example11" className="form-control"
-                      placeholder="Phone number or email address" />
-                    <label className="form-label" for="form2Example11">Username</label>
-                  </div>
-
-                  <div className="form-outline mb-4">
-                    <input type="password" id="form2Example22" className="form-control" />
-                    <label className="form-label" for="form2Example22">Password</label>
-                  </div>
-
-                  <div className="text-center pt-1 mb-5 pb-1">
-                    <button className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
-                      in</button>
-                    <a className="text-muted" href="#!">Forgot password?</a>
-                  </div>
-
-                  <div className="d-flex align-items-center justify-content-center pb-4">
-                    <p className="mb-0 me-2">Don't have an account?</p>
-                    <button type="button" className="btn btn-outline-danger">Create new</button>
-                  </div>
-
-                </form>
-
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+    </section>
     )
-    return (
+    // return (
 
-        <div classNameName="row">
-            <h2>Login</h2>
-            <form classNameName="col s12" onSubmit={submitHandler}>
-                <div classNameName="row">
-                    <div classNameName="input-field col s12">
-                        <input id="email" type="email" classNameName="validate"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                        <div classNameName="email error red-text">{emailError}</div>
-                        <label htmlFor="email">Email</label>
-                    </div>
-                </div>
-                <div classNameName="row">
-                    <div classNameName="input-field col s12">
-                        <input id="password" type="password" classNameName="validate"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                        <div classNameName="password error red-text">{passwordError}</div>
-                        <label htmlFor="password">Password</label>
-                    </div>
-                </div>
+    //     <div className="row">
+    //         <h2>Login</h2>
+    //         <form classNameName="col s12" onSubmit={submitHandler}>
+    //             <div classNameName="row">
+    //                 <div classNameName="input-field col s12">
+    //                     <input id="email" type="email" classNameName="validate"
+    //                         value={email}
+    //                         onChange={e => setEmail(e.target.value)}
+    //                     />
+    //                     <div classNameName="email error red-text">{emailError}</div>
+    //                     <label htmlFor="email">Email</label>
+    //                 </div>
+    //             </div>
+    //             <div classNameName="row">
+    //                 <div classNameName="input-field col s12">
+    //                     <input id="password" type="password" classNameName="validate"
+    //                         value={password}
+    //                         onChange={e => setPassword(e.target.value)}
+    //                     />
+    //                     <div classNameName="password error red-text">{passwordError}</div>
+    //                     <label htmlFor="password">Password</label>
+    //                 </div>
+    //             </div>
 
-                <button classNameName="btn">Login</button>
-            </form>
-        </div>
+    //             <button classNameName="btn">Login</button>
+    //         </form>
+    //     </div>
 
-    )
+    // )
 }
 //hej på dej
 export default Login
